21bafa7b92212187c598b4931670ecb95aeeaef9
mehmeterozkan91 <92365223+mehmeterozkan91@users.noreply.github.com>
2021-11-05 15:46:07 +0300

    java spring graphql

---
+++ javaspringboot-examples/spring-boot-graphql/src/main/java/com/spring/graphql/Application.java
@@ -0,0 +1,13 @@
+package com.spring.graphql;
+
+/**
+ **   User : Mehmet ERÖZKAN
+ **   Date : 05/11/2021
+ **   Time : 3:39 PM
+ **/
+
+public class Application {
+	public static void main(String[] args) {
+
+	}
+}
