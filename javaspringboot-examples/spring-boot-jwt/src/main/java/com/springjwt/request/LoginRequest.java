e636d9d18a7fab139552aad6cc74aec39e7466ec
mehmeterozkan91 <92365223+mehmeterozkan91@users.noreply.github.com>
2021-11-19 17:40:09 +0300

    java spring facebook login

--- javaspringboot-examples/spring-boot-jwt/src/main/java/com/springjwt/request/LoginRequest.java
+++ javaspringboot-examples/spring-boot-jwt/src/main/java/com/springjwt/request/LoginRequest.java
@@ -10,8 +10,6 @@ import lombok.Data;
 @Data
 public class LoginRequest {
 
-	private String email;
-
 	private String username;
 
 	private String password;
@@ -38,13 +36,5 @@ public class LoginRequest {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 }
